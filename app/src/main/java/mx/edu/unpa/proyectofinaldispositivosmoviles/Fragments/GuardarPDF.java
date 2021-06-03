de380c933b3685f99da9de3f32a75f795a699662
daniellin12 <84788918+daniellin12@users.noreply.github.com>
2021-06-03 10:04:45 -0500

    fragments parte2 adaptada al proyecto

--- app/src/main/java/mx/edu/unpa/proyectofinaldispositivosmoviles/Fragments/GuardarPDF.java
+++ app/src/main/java/mx/edu/unpa/proyectofinaldispositivosmoviles/Fragments/GuardarPDF.java
@@ -12,10 +12,10 @@ import mx.edu.unpa.proyectofinaldispositivosmoviles.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link RedFragment#newInstance} factory method to
+ * Use the {@link GuardarPDF#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RedFragment extends Fragment {
+public class GuardarPDF extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class RedFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public RedFragment() {
+    public GuardarPDF() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class RedFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RedFragment.
+     * @return A new instance of fragment GuardarPDF.
      */
     // TODO: Rename and change types and number of parameters
-    public static RedFragment newInstance(String param1, String param2) {
-        RedFragment fragment = new RedFragment();
+    public static GuardarPDF newInstance(String param1, String param2) {
+        GuardarPDF fragment = new GuardarPDF();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class RedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_red, container, false);
+        return inflater.inflate(R.layout.fragment_guardar_p_d, container, false);
     }
 }
--- app/src/main/java/mx/edu/unpa/proyectofinaldispositivosmoviles/MainActivity.java
+++ app/src/main/java/mx/edu/unpa/proyectofinaldispositivosmoviles/MainActivity.java
@@ -1,51 +1,37 @@
 package mx.edu.unpa.proyectofinaldispositivosmoviles;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.Manifest;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import java.io.File;
-
-import mx.edu.unpa.proyectofinaldispositivosmoviles.Fragments.BlueFragment;
-import mx.edu.unpa.proyectofinaldispositivosmoviles.Fragments.RedFragment;
-
-import static android.widget.Toast.LENGTH_SHORT;
+import mx.edu.unpa.proyectofinaldispositivosmoviles.Fragments.GuardarPDF;
+import mx.edu.unpa.proyectofinaldispositivosmoviles.Fragments.cargarPDF;
 
 public class MainActivity extends AppCompatActivity {
     FragmentTransaction transaction;
-    BlueFragment blueFragment;
-    RedFragment redFragment;
+    cargarPDF cargarPDF;
+    GuardarPDF guardarPDF;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //
-        blueFragment = new BlueFragment();
-        redFragment  = new RedFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.idFragments, blueFragment).commit();
+        guardarPDF = new GuardarPDF();
+        cargarPDF  = new cargarPDF();
 
     }
     public void OnClik(View view){
         transaction = getSupportFragmentManager().beginTransaction();
         switch(view.getId()){
-            case R.id.blue:
-                transaction.replace(R.id.idFragments,blueFragment).commit();
+            case R.id.guardar:
+                transaction.replace(R.id.idFragments,guardarPDF).commit();
+                transaction.addToBackStack(null);
                 break;
-            case R.id.red:
-                transaction.replace(R.id.idFragments,redFragment).commit();
+            case R.id.buscar:
+                transaction.replace(R.id.idFragments,cargarPDF).commit();
+                transaction.addToBackStack(null);
                 break;
             default:
         }
